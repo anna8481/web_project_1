049d4d30f85a72b247d43ea2c610e0823933b773
daye <dy92520@gmail.com>
2023-02-02 08:57:00 +0900

    230202 .env 파일 설정

--- back/index.js
+++ back/index.js
@@ -1,23 +1,23 @@
-const { app } = require('./src/app');
+require("dotenv").config();
+const { app } = require("./src/app");
 
-const port = 5000;
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`);
 });
-console.log(1);
-// mongoose 받아오기
-const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', true); // stricQuery 오류를 처리하기 위한 세팅
+// mongoose 받아오기
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", true); // stricQuery 오류를 처리하기 위한 세팅
+
+const DB_URL = process.env.MONGODB_URL || "address error";
 
 //mongo db atlas 연결
-mongoose
-  .connect(
-    'mongodb+srv://moTeam:bxOVnhCekl12R3lL@moteam3.pkqnbhq.mongodb.net/?retryWrites=true&w=majority'
-  )
-  .then(() => {
-    console.log('welcome');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoose.connect(DB_URL);
+const db = mongoose.connection;
+
+db.on("connected", () => console.log("welcome mongoDB" + DB_URL));
+db.on("error", (error) =>
+  console.error("\nfailed connection\n" + DB_URL + "\n" + error)
+);
