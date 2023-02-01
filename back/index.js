f23be3707efa2c83c4c73962bec2118df1aa2460
epitone <epitoneplus@gmail.com>
2023-02-01 21:43:29 +0900

    test: git 테스트

--- back/index.js
+++ back/index.js
@@ -1,22 +1,22 @@
-const { app } = require("./src/app");
+const { app } = require('./src/app');
 
 const port = 5000;
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
-
+console.log(1);
 // mongoose 받아오기
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.set("strictQuery", true); // stricQuery 오류를 처리하기 위한 세팅
+mongoose.set('strictQuery', true); // stricQuery 오류를 처리하기 위한 세팅
 
 //mongo db atlas 연결
 mongoose
   .connect(
-    "mongodb+srv://moTeam:bxOVnhCekl12R3lL@moteam3.pkqnbhq.mongodb.net/?retryWrites=true&w=majority"
+    'mongodb+srv://moTeam:bxOVnhCekl12R3lL@moteam3.pkqnbhq.mongodb.net/?retryWrites=true&w=majority'
   )
   .then(() => {
-    console.log("welcome");
+    console.log('welcome');
   })
   .catch((err) => {
     console.log(err);
