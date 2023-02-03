872f196b145f6b938a42c2cbab1e0760765894c1
daye <dy92520@gmail.com>
2023-02-03 18:14:12 +0900

    230203 합병 test

--- back/src/db/schemas/productSchema.js
+++ back/src/db/schemas/productSchema.js
@@ -6,24 +6,11 @@ const ProductSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    // sellerId: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "users",
-    //   required: true,
-    // },
     categoryId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "categorys",
       required: true,
     },
-    // manufacturer: {
-    //   type: String,
-    //   required: true,
-    // },
-    // shortDescription: {
-    //   type: String,
-    //   required: true,
-    // },
     productInfo: {
       type: String,
       required: true,
@@ -32,36 +19,10 @@ const ProductSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    // inventory: {
-    //   type: Number,
-    //   min: 0,
-    //   default: 10,
-    //   required: true,
-    // },
     price: {
       type: Number,
       required: true,
     },
-    // searchKeywords: {
-    //   type: [String],
-    //   required: true,
-    // },
-    // isRecommended: {
-    //   type: Boolean,
-    //   default: false,
-    //   required: false,
-    // },
-    // discountPercent: {
-    //   type: Number,
-    //   min: 0,
-    //   max: 95,
-    //   default: 0,
-    //   required: false,
-    // },
-    // sku: {
-    //   type: String,
-    //   required: false,
-    // },
   },
   {
     collection: "products",
--- back/src/routes/productRouter.js
+++ back/src/routes/productRouter.js
@@ -1,59 +1,159 @@
-const { model } = require("mongoose");
-const { ProductSchema } = require("../schemas/productSchema");
+import { Router } from "express";
+const loginRequired = require("../middlewares");
 
-const Product = model("products", ProductSchema);
+const { productService } = require("../services/productService");
 
-class ProductModel {
-  async findByProductName(productName) {
-    const product = await Product.findOne({ productName });
-    return product;
+const productRouter = Router();
+
+productRouter.post("/product", loginRequired, async (req, res, next) => {
+  try {
+    // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
+    }
+
+    // req (request) 에서 데이터 가져오기
+    const title = req.body.title;
+    const categoryId = req.body.categoryId;
+    const sellerId = req.currentUserId;
+    const manufacturer = req.body.manufacturer;
+    const shortDescription = req.body.shortDescription;
+    const detailDescription = req.body.detailDescription;
+    const imageKey = req.body.imageKey;
+    const inventory = req.body.inventory;
+    const price = req.body.price;
+    const searchKeywords = req.body.searchKeywords;
+
+    // 위 데이터를 제품 db에 추가하기
+    const newProduct = await productService.addProduct({
+      title,
+      categoryId,
+      sellerId,
+      manufacturer,
+      shortDescription,
+      detailDescription,
+      imageKey,
+      inventory,
+      price,
+      searchKeywords,
+    });
+
+    res.status(201).json(newProduct);
+  } catch (error) {
+    next(error);
   }
+});
 
-  // async findById(productId) {
-  //   const product = await Product.findOne({ _id: productId });
-  //   return product;
-  // }
+productRouter.get(
+  "/productlist",
+  loginRequired,
+  async function (req, res, next) {
+    try {
+      // 전체 제품 목록을 얻음
+      const products = await productService.getProducts();
 
-  async findOneByCategoryId(categoryId) {
-    const product = await Product.findOne({ categoryId });
-    return product;
+      res.status(200).json(products);
+    } catch (error) {
+      next(error);
+    }
   }
+);
 
-  async findAllByCategoryId(categoryId) {
-    const products = await Product.find({ categoryId });
-    return products;
+productRouter.get(
+  "/productlist/category/:categoryTitle",
+  async function (req, res, next) {
+    let categoryTitle = req.params.categoryTitle;
+
+    try {
+      // 전체 제품 목록을 얻음
+      const products = await productService.getProductsByCategoryTitle(
+        categoryTitle
+      );
+
+      res.status(200).json(products);
+    } catch (error) {
+      next(error);
+    }
   }
+);
 
-  // async create(productInfo) {
-  //   const createdNewProduct = await Product.create(productInfo);
-  //   return createdNewProduct;
-  // }
+productRouter.get("/products/:productId", async function (req, res, next) {
+  try {
+    const productId = req.params.productId;
+    const productData = await productService.getProductData(productId);
 
-  async findAll() {
-    const products = await Product.find({});
-    return products;
+    res.status(200).json(productData);
+  } catch (error) {
+    next(error);
   }
+});
 
-  // async update({ productId, update }) {
-  //   const filter = { _id: productId };
-  //   const option = { returnOriginal: false };
+productRouter.patch(
+  "/products/:productId",
+  loginRequired,
+  async function (req, res, next) {
+    try {
+      // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
+      if (is.emptyObject(req.body)) {
+        throw new Error(
+          "headers의 Content-Type을 application/json으로 설정해주세요"
+        );
+      }
 
-  //   const updatedProduct = await Product.findOneAndUpdate(
-  //     filter,
-  //     update,
-  //     option
-  //   );
-  //   return updatedProduct;
-  // }
+      // req (request) 에서 데이터 가져오기
+      const productId = req.params.productId;
+      const title = req.body.title;
+      const shortDescription = req.body.shortDescription;
+      const detailDescription = req.body.detailDescription;
+      const imageKey = req.body.imageKey;
+      const inventory = req.body.inventory;
+      const price = req.body.price;
+      const searchKeywords = req.body.searchKeywords;
+      const isRecommended = req.body.isRecommended;
+      const discountPercent = req.body.discountPercent;
 
-  async deleteById(productId) {
-    const result = await Product.deleteOne({ _id: productId });
-    return result;
+      // 위 데이터가 undefined가 아니라면, 즉, 프론트에서 업데이트를 위해
+      // 보내주었다면, 업데이트용 객체에 삽입함.
+      const toUpdate = {
+        ...(title && { title }),
+        ...(shortDescription && { shortDescription }),
+        ...(detailDescription && { detailDescription }),
+        ...(imageKey && { imageKey }),
+        ...(inventory && { inventory }),
+        ...(price && { price }),
+        ...(searchKeywords && { searchKeywords }),
+        ...(isRecommended && { isRecommended }),
+        ...(discountPercent && { discountPercent }),
+      };
+
+      // 제품 정보를 업데이트함.
+      const updatedProduct = await productService.setProduct(
+        productId,
+        toUpdate
+      );
+
+      res.status(200).json(updatedProduct);
+    } catch (error) {
+      next(error);
+    }
   }
-}
+);
 
-module.exports = ProductModel;
+productRouter.delete(
+  "/products/:productId",
+  loginRequired,
+  async function (req, res, next) {
+    try {
+      const productId = req.params.productId;
+      const deleteResult = await productService.deleteProductData(productId);
 
-const productModel = new ProductModel();
+      res.status(200).json(deleteResult);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-module.exports = { productModel };
+export { productRouter };
