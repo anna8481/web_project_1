f27ebf5d2016f88d4e72a017029c89b80260ed8d
anna8481 <nahn8481@gmail.com>
2023-02-08 18:30:08 +0900

    git test

--- front/src/pages/cart/OrderComplete.js
+++ front/src/pages/cart/OrderComplete.js
@@ -9,16 +9,6 @@ function OrderComplete() {
     return (
         <>
             <div className="container">
-                <MDBBreadcrumb>
-                    <MDBBreadcrumbItem>
-                        <Link to='/cart'>장바구니</Link>
-                    </MDBBreadcrumbItem>
-                    <MDBBreadcrumbItem>
-                        <Link to="/order">주문결제</Link>
-                    </MDBBreadcrumbItem>
-                    <MDBBreadcrumbItem active>주문완료</MDBBreadcrumbItem>
-                </MDBBreadcrumb>
-
                 <div className="order-complete">
                     <div className="order-complete-header"><h3>Thank you for purchasing!</h3></div>
                     <Link to={ROUTE.ACCOUNT_ORDERS.link}>
