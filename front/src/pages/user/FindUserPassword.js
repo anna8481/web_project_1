8dc3ab7011322e7a13a438bfb8150df0e91d6fa4
SinHT0108 <somu950108@gmail.com>
2023-02-09 19:52:45 +0900

    find Password

--- front/src/pages/user/FindUserPassword.js
+++ front/src/pages/user/FindUserPassword.js
@@ -5,52 +5,44 @@ import { Link } from 'react-router-dom';
 import { ROUTE } from '../../utills/route';
 
 
-function FindUserPassword () {
+function FindUserPassword() {
 
     const navigate = useNavigate();
     const [userEmail, setUserEmail] = useState("");
 
     const handleChange = event => {
-        setUserEmail(() => 
+        setUserEmail(() =>
             event.target.value
         )
     }
 
     const lostpass = async (e) => {
         e.preventDefault();
-        console.log(userEmail)
-        
+
+        const formdata = {
+            userEmail
+        }
+        console.log(formdata)
         try {
-            const response = await Api.post("users/help/password", {
-                userEmail
-            });
-            // localStorage.setItem('email', response.data.email);
-
-            // if (response.data.email === userEmail) {
-            //     alert("이메일을 보냈습니다.");
-            // }
-            // else {
-            //     alert("가입되지 않은 이메일입니다.");
-            // }
-
+            const response = await Api.post("users/help/password", formdata)
+                ;
+            alert("가입하신 이메일로 임시 비밀번호를 전송했습니다.")
+            navigate('/login');
         } catch (err) {
-            alert("에러가 발생했습니다.")
+            alert("가입된 정보가 없습니다.");
         }
     };
 
-    return(
+    return (
         <div>
-            <form onSubmit={""} className="user-form">
+            <form onSubmit={lostpass} className="user-form">
                 <h4>비밀번호 찾기</h4>
                 <p>임시 비밀번호 발급</p>
                 <p>이메일 <input className="email" onChange={handleChange} value={userEmail} /></p>
-                <button onClick={lostpass}>전송</button>
+                <button type="submit">전송</button>
                 <p>이메일에 전송된 비밀번호를 확인 후 다시 로그인 해 주세요.</p>
-                <button>
-                    <Link to={ROUTE.LOGIN.link}>로그인할게요</Link>
-                </button>
-            </form>
 
+            </form>
         </div>
     )
 }
