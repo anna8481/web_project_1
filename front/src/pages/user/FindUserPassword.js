5a2a88a7005e6fc63c55e7fb639bfd5f2e8fadea
anna8481 <nahn8481@gmail.com>
2023-02-09 20:14:04 +0900

    add css style

--- front/src/pages/user/FindUserPassword.js
+++ front/src/pages/user/FindUserPassword.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom';
 import { ROUTE } from '../../utills/route';
-
+import Header from '../../components/Header'
 
 function FindUserPassword() {
 
@@ -34,16 +34,37 @@ function FindUserPassword() {
     };
 
     return (
-        <div>
-            <form onSubmit={lostpass} className="user-form">
-                <h4>비밀번호 찾기</h4>
-                <p>임시 비밀번호 발급</p>
-                <p>이메일 <input className="email" onChange={handleChange} value={userEmail} /></p>
-                <button type="submit">전송</button>
-                <p>이메일에 전송된 비밀번호를 확인 후 다시 로그인 해 주세요.</p>
+        // <div>
+        //     <form onSubmit={lostpass} className="user-form">
+        //         <h4>비밀번호 찾기</h4>
+        //         <p>임시 비밀번호 발급</p>
+        //         <p>이메일 <input className="email" onChange={handleChange} value={userEmail} /></p>
+        //         <button type="submit">전송</button>
+        //         <p>이메일에 전송된 비밀번호를 확인 후 다시 로그인 해 주세요.</p>
 
-            </form>
+        //     </form>
+        // </div>
+
+
+        <div className="section">
+            <Header title="비밀번호 찾기"></Header>
+            <div className="container-center">
+                <p style={{ textAlign: "center" }}>임시 비밀번호 발급</p>
+                <form className="user-form">
+
+                    <div style={{ margin: "1rem" }}></div>
+                    <div>
+                        <label>이메일</label>
+                    </div>
+                    <input className="input" label='' name='password' type='password' placeholder="비밀번호를 입력하세요" value={userEmail} onChange={handleChange} />
+                    <button className="user-button" onClick={lostpass} >
+                        이메일 전송하기
+                    </button>
+                </form>
+                <p style={{ marginTop: "1rem" }}>이메일에 전송된 비밀번호를 확인 후 다시 로그인 해 주세요.</p>
+            </div>
         </div>
+
     )
 }
 
