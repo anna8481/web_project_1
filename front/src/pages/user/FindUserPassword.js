06f8be93dbb43866f8bdfdcdb98c6c49821be6f5
seungeun Lee <dltmddms0819@gmail.com>
2023-02-09 19:32:38 +0900

    finduserpassword

--- front/src/pages/user/FindUserPassword.js
+++ front/src/pages/user/FindUserPassword.js
@@ -4,39 +4,47 @@ import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom';
 import { ROUTE } from '../../utills/route';
 
+
 function FindUserPassword () {
 
-    // const lostpass = async (e) => {
-    //     e.preventDefault();
-    //     const navigate = useNavigate();
-    //     const userEmail = inputs;
+    const navigate = useNavigate();
+    const [userEmail, setUserEmail] = useState("");
 
-    //     try {
-    //         const response = await Api.post("users/help/password", {
-    //             userEmail
-    //         });
-    //         localStorage.setItem('email', response.data.email);
+    const handleChange = event => {
+        setUserEmail(() => 
+            event.target.value
+        )
+    }
 
-    //         if (response.data.email === userEmail) {
-    //             "메일로 임시비밀번호 보내기";
-    //         }
-    //         else {
-    //             navigate(ROUTE.LOGIN.link);
-    //         }
+    const lostpass = async (e) => {
+        e.preventDefault();
+        console.log(userEmail)
+        
+        try {
+            const response = await Api.post("users/help/password", {
+                userEmail
+            });
+            // localStorage.setItem('email', response.data.email);
 
-    //         console.log(response)
-    //     } catch (err) {
-    //         alert("가입되지 않은 이메일입니다.")
-    //     }
-    // };
+            // if (response.data.email === userEmail) {
+            //     alert("이메일을 보냈습니다.");
+            // }
+            // else {
+            //     alert("가입되지 않은 이메일입니다.");
+            // }
+
+        } catch (err) {
+            alert("에러가 발생했습니다.")
+        }
+    };
 
     return(
         <div>
             <form onSubmit={""} className="user-form">
                 <h4>비밀번호 찾기</h4>
                 <p>임시 비밀번호 발급</p>
-                <p>이메일 <input className="email"/></p>
-                <button>전송</button>
+                <p>이메일 <input className="email" onChange={handleChange} value={userEmail} /></p>
+                <button onClick={lostpass}>전송</button>
                 <p>이메일에 전송된 비밀번호를 확인 후 다시 로그인 해 주세요.</p>
                 <button>
                     <Link to={ROUTE.LOGIN.link}>로그인할게요</Link>
