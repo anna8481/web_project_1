5bab66ed713ec063ba4abede9843e5a340a0b722
anna8481 <nahn8481@gmail.com>
2023-02-16 09:57:53 +0900

    gitignore env

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import React from 'react';
 import './App.css';
