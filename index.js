a83fb6d2ae90b6662b9eaff7e60a72824c9cd507
anna8481 <nahn8481@gmail.com>
2024-01-04 12:14:36 +0900

    fix merge error

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-require("dotenv").config();
-const logger = require("./src/utils/logger");
-const { app } = require("./src/app");
+require('dotenv').config();
+const logger = require('./src/utils/logger');
+const { app } = require('./src/app');
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`);
 });
 
-
 // mongoose 받아오기
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.set("strictQuery", true); // stricQuery 오류를 처리하기 위한 세팅
+mongoose.set('strictQuery', true); // stricQuery 오류를 처리하기 위한 세팅
 
-const DB_URL = process.env.MONGODB_URL || "address error";
+const DB_URL = process.env.MONGODB_URL || 'address error';
 
 //mongo db atlas 연결
 mongoose.connect(DB_URL);
 const db = mongoose.connection;
 
-db.on("connected", () => console.log("welcome mongoDB" + DB_URL));
-db.on("error", (error) => logger.error("failed connection\n" + error));
+db.on('connected', () => console.log('welcome mongoDB'));
+db.on('error', (error) => logger.error('failed connection\n' + error));
