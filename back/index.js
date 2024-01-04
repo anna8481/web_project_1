bcc6871b750e3f9c7f1ead1af121bc15b285271c
anna <120162498+anna8481@users.noreply.github.com>
2024-01-04 12:22:02 +0900

    Update index.js

--- back/index.js
+++ back/index.js
@@ -1,11 +1,7 @@
 require("dotenv").config();
 const logger = require("./src/utils/logger");
 const { app } = require("./src/app");
-
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-  console.log(`app listening on port ${PORT}`);
-});
 
 
 // mongoose 받아오기
@@ -15,9 +11,20 @@ mongoose.set("strictQuery", true); // stricQuery 오류를 처리하기 위한 �
 
 const DB_URL = process.env.MONGODB_URL || "address error";
 
-//mongo db atlas 연결
-mongoose.connect(DB_URL);
-const db = mongoose.connection;
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(DB_URL);
+    console.log(`mongoDB connected: + ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    logger.error('failed connection\n' + error);
+    process.exit(1);
+  }
+};
 
-db.on("connected", () => console.log("welcome mongoDB" + DB_URL));
-db.on("error", (error) => logger.error("failed connection\n" + error));
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`app listening on port ${PORT}`);
+  });
+});
+
